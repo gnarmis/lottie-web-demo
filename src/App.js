2496fab443183a7e463ac1e7a5ba62cf6025cb73
Gursimran Singh <g@kilotau.com>
2017-06-21 18:18:33 -0500

    Basic demo with a custom lottie file

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Lottie from 'react-lottie';
+import * as animationData from "./favorite.json";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {isStopped: false, isPaused: false};
+  }
+
+  render() {
+    const buttonStyle = {
+      display: 'block',
+      margin: '10px auto'
+    };
+    const defaultOptions = {animationData: animationData};
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <Lottie options={defaultOptions}
+              height={400}
+              width={400}
+              isStopped={this.state.isStopped}
+              isPaused={this.state.isPaused}/>
+        <button style={buttonStyle} onClick={() => this.setState({isStopped: true})}>stop</button>
+        <button style={buttonStyle} onClick={() => this.setState({isStopped: false})}>play</button>
+        <button style={buttonStyle} onClick={() => this.setState({isPaused: !this.state.isPaused})}>pause</button>
+      </div>
+    );
+  }
+}
+
+export default App;
